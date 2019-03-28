682d95395650ae6693695d30eea68a514547274c
UnknownMonk <smodcartoon@gmail.com>
2019-03-28 19:12:45 -0400

    added dish id modle

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 
-// const db = require();
+const dishesRoute = require('./dishes/dishes_router');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use('/api/dishes', dishesRoute);
 
 const port = process.env.PORT || 5000;
 server.listen(port, () =>
